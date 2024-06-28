3ba9e6531a46f227e70c61da4e058b2f97d76632
Himanshu1110 <himanshufulwani1990@gmail.com>
2024-06-28 16:09:08 +0530

    Pod, NetworkManger and Constant Added

---
+++ ShopMe-Github-Collab/Constants/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  ShopMe-Github-Collab
+//
+//  Created by Webcodegenie on 28/06/24.
+//
+
+import Foundation
+let headerContentType =  "Content-Type"
+let contentTypeUrlJSON =  "application/json"
+
+//let API_BASE_URL = "https://reqres.in/api/"
+
+class Constant {
+//    static let GET_USER_LIST = API_BASE_URL + "users?page=1"
+}
